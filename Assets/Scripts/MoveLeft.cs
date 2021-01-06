9db62de6ad7d815201c3d53db2630611fbc2e4ae
josse995 <jomore03@ucm.es>
2021-01-06 01:22:59 +0100

    Added new functionalities
    Move background on loop
    Added particles
    Action's sound (jump, died)
    More...

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+
+    public float speed = 30;
+    private PlayerController playerControllerScript;
+
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!playerControllerScript.gameOver)
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnObstacle), startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    private void SpawnObstacle()
+    {
+        if (!playerControllerScript.gameOver)
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        else
+            Debug.Log("GAMEOVER");
+    }
+}
